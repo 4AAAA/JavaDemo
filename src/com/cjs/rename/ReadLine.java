9fe74f6282fe29a198082b8c641b5fa883745f82
liuyw <348590506@qq.com>
2017-09-15 14:30:30 +0800

    javaDemo:新增其他厂商重命名功能

--- src/com/cjs/rename/ReadLine.java
+++ src/com/cjs/rename/ReadLine.java
@@ -25,6 +25,8 @@ public class ReadLine {
 			System.out.println("警告:原文件目录为空");		
 		//迭代目录所有文件
 		for (File temp : dir.listFiles()) {
+			//游标(判断文本是否停止读取，所有文本一旦获关键值后则把游标赋值为1标记终止)
+			int ver = 0;
 			// 获取后缀名
 			String fullName = temp.getName();
 			String lastName = fullName.substring(fullName.lastIndexOf(".") + 1);
@@ -32,33 +34,59 @@ public class ReadLine {
 			if (temp.isFile() && lastName.equals("log")) {
 
 				file = new File(temp.toString());		
-				BufferedReader reader = null;
+				BufferedReader reader1 = null;
+				BufferedReader reader2 = null;
 				
 				//定义目标行和重命名
 				String target = "无法识别目标，请检查文本是否在规则范围内";
 				String rename = "未定义";
 
 				try {
-					reader = new BufferedReader(new FileReader(file));
+					
 					String tempString = null;
-					int line = 1;
-					// 一次读入一行，直到读入null为文件结束
-					while ((tempString = reader.readLine()) != null) {
-						// 判断是否存在关键字：sysname
+					String factory="other";
+					
+					//第一次扫描:确定是否为华为厂商
+					reader1 = new BufferedReader(new FileReader(file));
+					// 一次读入一行，直到读入null并且游标终止时为文件结束，第一次扫描先确定华为厂商，排除'A'和'B'干扰
+					while (ver==0 && (tempString = reader1.readLine()) != null) {
+						// 华为厂商：判断是否存在关键字：sysname
 						if (tempString.contains("sysname")) {
 							target = tempString;
 							// 截取所需的文件名
-							rename = tempString.substring(tempString.indexOf("e") + 1, tempString.length());
+							rename = tempString.substring(tempString.indexOf("e") + 2, tempString.length());
+							//游标标记终止，则后面不再往下读取文本
+							ver=1;
+							//华为厂商
+							factory="HuaWei";
 						}
-						line++;
+						
 					}
-					reader.close();
+					reader1.close();
+					
+					//第二次扫描：先判断如果不是华为厂商才进行其他厂商的扫描
+					reader2 = new BufferedReader(new FileReader(file));
+					// 一次读入一行，直到读入null并且游标终止时为文件结束，第一次扫描先确定华为厂商，排除'A'和'B'干扰
+					while (ver==0 & factory.equals("other") & (tempString = reader2.readLine()) != null) {
+						//其他厂商：判断是否有'A'和'B'关键字，如果是华为厂商则不能执行该判断（用游标判断是否已经使用过华为判断规则）
+						if(ver!=1&&tempString.contains("A")||tempString.contains("B")) {
+							target = tempString;
+							// 截取所需的文件名
+							rename = tempString.substring(tempString.indexOf(":") + 1, tempString.lastIndexOf("#"));
+							//游标标记终止，则后面不再往下读取文本
+							ver=1;
+						}
+						
+					}
+					reader2.close();
+					
+										
 					// 重命名后的文件
 					File file1 = new File("/Users/liuyw/Desktop/newFile/" + rename + ".log");
 					// 重命名文件
 					file.renameTo(file1);
 					// 输出日志信息
-					System.out.println("文件序号：" + num + "\r\n原文件:" + fullName.substring(fullName.lastIndexOf("/") + 1)
+					System.out.println("文件序号：" + num + "\r\n厂商:"+factory+"\r\n原文件:" + fullName.substring(fullName.lastIndexOf("/") + 1)
 							+ "\r\n目标行:" + target + "\r\n重命名文件:"
 							+ file1.getName().substring(file1.getName().lastIndexOf("/") + 1)
 							+ "\t\n---------------------------------------------------------------------");
@@ -69,9 +97,10 @@ public class ReadLine {
 					e.printStackTrace();
 					System.out.println("代码发生异常，请联系开发人员！");
 				} finally {
-					if (reader != null) {
+					if (reader1 != null) {
 						try {
-							reader.close();
+							reader1.close();
+							reader2.close();
 						} catch (IOException e1) {
 						}
 					}
